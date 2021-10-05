6a1e62228f7308bad8e10343659a430602b3ba11
Michael Burman <michaelaburman@gmail.com>
2021-10-04 17:19:30 -0700

    Update xlsx-to-csv.py

--- xlsx-to-csv.py
+++ xlsx-to-csv.py
@@ -9,7 +9,12 @@ Policy Number, LOB, Policy Effective Date, Effective Year, Claim Number, Loss Da
 Accident Description, Claim Status, Total Reserved, Total Paid, Total Incurred, Recovered,
 Filename, Valuation Date
 """
+print("hello")
 import pandas as pd
+
+hrow = int(input("On which row do the headers begin? (Number only)"))-1
+hcol = input("On which column does the data begin? (Letter)").upper()
+
 
 simple = ['Policy Number', 'LOB', 'Policy Effective Date', 'Effective Year', 'Claim Number', 'Loss Date', 'Report Date', 
 'Accident Description', 'Claim Status', 'Total Reserved', 'Total Paid', 'Total Incurred', 'Recovered',
@@ -22,42 +27,58 @@ expanded = ['Policy Number', 'LOB', 'Policy Effective Date', 'Effective Year', '
 
 def read_data():
     '''Returns a dataframe from a converted .xlsx->CSV as a pandas dataframe.'''
-    data = pd.read_excel(input("What is the name of the file?")+".xlsx")
+    data = pd.ExcelFile(input("What is the name of the file?")+".xlsx")
     #csv_name = input("What is the name of the desired .csv?")+".csv"
     #data.to_csv(csv_name, encoding="utf-8")
     #data = pd.read_csv(csv_name)
+    if len(data.sheet_names) > 1: 
+        print("Warning: This Excel file has multiple sheets. There are "+len(data.sheet_names)+" in the Excel file.")
     return data
 
-detailtype = input("Simple or Expanded loss data?").lower()
+detailtype = input("Simple or Expanded loss data? (TYPE 'S' OR 'E'. ").lower()
 
-def rename_cols(df, detailtype):
+def merge_sheets(data):
     '''
     Inputs: 
-    df: dataframe, pandas object
-    detailtype: List of columns used for CSV output.
+    data: Excel sheet as a pandas object
     
-    Renames columns from inputted .xlsx and returns a dictionary with the new column names.
+    Organizes sheet names from the inputted excel spreadsheet then concatenates them into single dataframe.
     '''
-    renamed_cols = []
-    #columns from csv that need to be renamed
-    rename_dict = {}
-    #dictionary for use in pd.df.rename
-    for col_name in df.columns:
-        resp = input("Should " + col_name + " be renamed? y/n: ").upper()
-        if resp != "N":
-            renamed_cols.append(col_name)
-    for col_name in renamed_cols:
-        rename_dict[col_name] = input("What should " + col_name + " be called?")
-    df.rename(columns=rename_dict)
+    dflist = []
+    tab_names = data.sheet_names
+    if len(tab_names) > 1:
+        for tab in tab_names:
+            df = data.parse(sheetname=tab, skiprows=hrow) #parses data sheet and adds it to a list for concatenation
+            dataframes.append(df)
+        df = pd.concat(dflist)
+    else: 
+        df = data.parse(skiprows=hrow)
+    print("------------------------------------------------------------------")
+    print(df.info())
     return df
 
-def new_csv(df):
+def extract_cols(df, detailtype):
     '''
     Inputs: 
+    df: Pandas dataframe
+    detailtype: Inputted strirng determining which columns are going to be utilized
+
+    Takes dataframe columns, creates a new dataframe, and joins the old dataframe to the new one, only taking the needed columns.
     '''
+    grabbed_cols = []
+    while True:
+        grabbed_col = input("Name exact column you would like to grab(type 'break' to exit): ")
+        if grabbed_col == "break":
+            break
+        else:
+            grabbed_cols.append(grabbed_col)
+    new_df = pd.dataframe
+    for col in grabbed_cols:
+        new_df[col] = df[col]
+    return new_df
 
 df = read_data()
-df = rename_cols(df, detailtype)
-new_csv(df)
+df = mergedata(df, detailtype)
+df =  
     
         
